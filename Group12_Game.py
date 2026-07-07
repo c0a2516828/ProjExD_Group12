8ccacd61700ae834cebde2276a2ea613977e7865
c0a25028cf <c0a25028cf@edu.teu.ac.jp>
2026-07-07 17:54:23 +0900

    enemyの実装

--- Group12_Game.py
+++ Group12_Game.py
@@ -38,13 +38,50 @@ class Player:
         pass
 
 class Enemy:
-    def __init__(self):
-        pass
-    def apper():
-        pass
-    def action():
-        pass
+    """
+    敵キャラを管理するクラス
+    """
+    def __init__(self, name, hp, atk,img_path,special=False):
+        self.name = name
+        self.hp = hp
+        self.atk = atk
+        self.special = special  # 特殊敵（攻撃されたらゲームオーバー）
+        # 敵画像を読み込む
+        self.image = pg.image.load(img_path)
 
+        # 画像位置：横中央、縦は上に張り付き
+        self.x = (WIDTH - self.image.get_width()) // 2
+        self.y = 0
+
+    @staticmethod
+    def apper():
+        """
+        敵を確率で出現させる
+        1: hp10 atk10（80%）
+        2: hp300 atk100（15%）
+        3: hp10000 atk0（5%）攻撃されたらゲームオーバー
+        """
+        r = random.random()
+
+        if r < 0.80:
+            # 80%
+            enemy = Enemy("廃れた像", 10, 10,"IMG_廃れた像.jpg")
+        elif r < 0.95:
+            # 15%
+            enemy = Enemy("黄金像", 300, 100,"IMG_黄金像.jpg")
+        else:
+            # 5%
+            enemy = Enemy("退学馬", 10000, 0,"IMG_退学馬.jpg", special=True)
+
+        Chat.sent(f"{enemy.name} が現れた！ HP:{enemy.hp} ATK:{enemy.atk}")
+        return enemy
+
+    def action(self, player):
+        """
+        敵の行動（攻撃）
+        """
+        Chat.sent(f"{self.name} の攻撃！ {self.atk} ダメージ！")
+        player.hp -= self.atk
 
 def main():
     pg.display.set_caption("ゲーム")
